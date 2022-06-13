08f8d20a789620046915752ac6c3521473d012e9
Thomas Santoro <thomas_santoro@ens.univ-artois.fr>
2022-06-13 11:23:37 +0200

    sftdzaueqfgjusde

--- src/test/java/fr/univartois/butinfo/sae/projetventes/model/article/ArticleCat2Test.java
+++ src/test/java/fr/univartois/butinfo/sae/projetventes/model/article/ArticleCat2Test.java
@@ -2,7 +2,6 @@ package fr.univartois.butinfo.sae.projetventes.model.article;
 
 
 import static org.assertj.core.api.Assertions.assertThat;
-import static org.junit.jupiter.api.Assertions.*;
 
 import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeEach;
