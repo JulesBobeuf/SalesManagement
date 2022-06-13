b2ff3b1a99ec3fff698de36ee478ba5a94c00f01
Thomas Santoro <thomas_santoro@ens.univ-artois.fr>
2022-06-13 16:51:00 +0200

    haaaaaaaaaaaaaaaaaaaaaaaaaaaaa

---
+++ src/test/java/fr/univartois/butinfo/sae/projetventes/model/commande/CommandeTest.java
@@ -0,0 +1,89 @@
+package fr.univartois.butinfo.sae.projetventes.model.commande;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+class CommandeTest {
+
+	@BeforeEach
+	void setUp() throws Exception {
+	}
+
+	@AfterEach
+	void tearDown() throws Exception {
+	}
+
+	@Test
+	void testCommande() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	void testEstPleine() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	void testEstVide() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	void testEstCloturee() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	void testSupprimerLigneCommande() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	void testCommander() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	void testGetNbLignesCommande() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	void testGetReference() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	void testGetMontantAvecLivraison() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	void testGetMontant() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	void testCloturer() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	void testGetArticle() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	void testGetQuantite() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	void testToString() {
+		fail("Not yet implemented");
+	}
+
+}
