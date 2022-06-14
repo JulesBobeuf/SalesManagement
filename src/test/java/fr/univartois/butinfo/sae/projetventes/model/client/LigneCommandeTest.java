e5618459151654299bb9e6b48d69c8ce74cbb1f9
Pierre Schreiner <pierre_schreiner@ens.univ-artois.fr>
2022-06-14 16:24:59 +0200

    ENFIN TERMIONay

--- src/test/java/fr/univartois/butinfo/sae/projetventes/model/client/LigneCommandeTest.java
+++ src/test/java/fr/univartois/butinfo/sae/projetventes/model/client/LigneCommandeTest.java
@@ -2,9 +2,6 @@ package fr.univartois.butinfo.sae.projetventes.model.client;
 
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.mockito.Mockito.when;
-
-import org.junit.jupiter.api.AfterEach;
-import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.Mock;
@@ -18,20 +15,12 @@ class LigneCommandeTest {
 	private LigneCommande ligne;
 	@Mock private Article a1;
 	
-	@BeforeEach
-	void setUp() throws Exception {
-		when(a1.getQuantiteStock()).thenReturn(100);
-		when(a1.getPrix()).thenReturn(3.0);
-		ligne = new LigneCommande(a1, 10);
-	}
-
-	@AfterEach
-	void tearDown() throws Exception {
-		ligne = null;
-	}
+	// On ne peut pas mettre de BeforeEach avec les when, sinon Junit lève une erreur pour when inutilisés.
 	
 	@Test
 	void testConstructeur() {
+		when(a1.getQuantiteStock()).thenReturn(100);
+		ligne = new LigneCommande(a1, 10);
 		assertThat(ligne).isInstanceOf(LigneCommande.class);
 		assertThat(ligne.getArticle()).isEqualTo(a1);
 		assertThat(ligne.getQuantite()).isEqualTo(10);
@@ -39,11 +28,17 @@ class LigneCommandeTest {
 	
 	@Test
 	void testGetMontant() {
+		when(a1.getQuantiteStock()).thenReturn(100);
+		when(a1.getPrix()).thenReturn(3.0);
+		ligne = new LigneCommande(a1, 10);
 		assertThat(ligne.getMontant()).isEqualTo(30);
 	}
 	
 	@Test
 	void testToString() {
+		when(a1.getQuantiteStock()).thenReturn(100);
+		when(a1.getPrix()).thenReturn(3.0);
+		ligne = new LigneCommande(a1, 10);
 		assertThat(ligne).hasToString("Réf. Art.: "+a1.getReference()+" ("+a1.getDesignation()+") | Qté.: "+ligne.getQuantite()+" | Prix U.: "+a1.getPrix()+" | Montant: "+ligne.getMontant()+" | Montant avec livr.: "+ligne.getMontantAvecLivraison());
 	}
 }
