1c51c7ce3409fed109663d01ab6a6d2e63949854
Thomas Santoro <thomas_santoro@ens.univ-artois.fr>
2022-06-14 10:38:55 +0200

    # WARNING: head commit changed in the meantime
    Merge branch 'main' of
    https://gitlab.univ-artois.fr/pierre_schreiner/sae-2.04-java.git into
    main

--- src/test/java/fr/univartois/butinfo/sae/projetventes/model/commande/CommandeTest.java
+++ src/test/java/fr/univartois/butinfo/sae/projetventes/model/commande/CommandeTest.java
@@ -1,34 +1,95 @@
 package fr.univartois.butinfo.sae.projetventes.model.commande;
 
+import static org.assertj.core.api.Assertions.assertThat;
 import static org.junit.jupiter.api.Assertions.*;
+import static org.mockito.Mockito.when;
 
 import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.Mock;
+import org.mockito.junit.jupiter.MockitoExtension;
+
+import fr.univartois.butinfo.sae.projetventes.model.article.Article;
+import fr.univartois.butinfo.sae.projetventes.model.client.Client;
+import fr.univartois.butinfo.sae.projetventes.model.client.ClientEntreprise;
+
+@ExtendWith(MockitoExtension.class)
 
 class CommandeTest {
 
+	private Commande commande1 ;
+	@Mock private Client client1;
+	@Mock Article article1;
+	@Mock Article article2;
+	@Mock Article article3;
+	@Mock Article article4;
+	@Mock Article article5;
+	@Mock Article article6;
+	@Mock Article article7;
+	@Mock Article article8;
+	@Mock Article article9;
+	@Mock Article article10;
+	@Mock Article article11;
+	@Mock Article article12;
+	@Mock Article article13;
+	@Mock Article article14;
+	
 	@BeforeEach
 	void setUp() throws Exception {
+		commande1 = new Commande(client1);
 	}
 
-	@AfterEach
+	@AfterEach 
 	void tearDown() throws Exception {
-	}
-
-	@Test
-	void testCommande() {
-		fail("Not yet implemented");
+		commande1 = null;
 	}
 
 	@Test
 	void testEstPleine() {
-		fail("Not yet implemented");
+		when(article1.getQuantiteStock()).thenReturn(100);
+		when(article1.getReference()).thenReturn(1);
+		when(article2.getQuantiteStock()).thenReturn(100);
+		when(article2.getReference()).thenReturn(2);
+		when(article3.getQuantiteStock()).thenReturn(100);
+		when(article3.getReference()).thenReturn(3);
+		when(article4.getQuantiteStock()).thenReturn(100);
+		when(article4.getReference()).thenReturn(4);
+		when(article5.getQuantiteStock()).thenReturn(100);
+		when(article5.getReference()).thenReturn(5);
+		when(article6.getQuantiteStock()).thenReturn(100);
+		when(article6.getReference()).thenReturn(6);
+		when(article7.getQuantiteStock()).thenReturn(100);
+		when(article7.getReference()).thenReturn(7);
+		when(article8.getQuantiteStock()).thenReturn(100);
+		when(article8.getReference()).thenReturn(8);
+		when(article9.getQuantiteStock()).thenReturn(100);
+		when(article9.getReference()).thenReturn(9);
+		when(article10.getQuantiteStock()).thenReturn(100);
+		when(article10.getReference()).thenReturn(10);
+		assertThat(commande1.estPleine()).isEqualTo(false);
+		commande1.commander(article1,2);
+		commande1.commander(article2,2);
+		commande1.commander(article3,2);
+		commande1.commander(article4,2);
+		commande1.commander(article5,2);
+		commande1.commander(article6,2);
+		commande1.commander(article7,2);
+		commande1.commander(article8,2);
+		commande1.commander(article9,2);
+		commande1.commander(article10,2);
+		assertThat(commande1.getNbLignesCommande()).isEqualTo(10);
+		assertThat(commande1.estPleine()).isEqualTo(true);
 	}
 
 	@Test
 	void testEstVide() {
-		fail("Not yet implemented");
+		assertTrue(commande1.estVide());
+		when(article1.getQuantiteStock()).thenReturn(100);
+		when(article1.getReference()).thenReturn(1);
+		commande1.commander(article1,2);
+		assertFalse(commande1.estVide());
 	}
 
 	@Test
